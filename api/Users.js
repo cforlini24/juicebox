6fd0045d1a2415e0221050ecd3b43425c8ab5b04
cforlini24 <cforlini24@gmail.com>
2023-03-17 13:52:21 -0400

    part 2.2

--- api/Users.js
+++ api/Users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const userRouter = express.Router();
-const {getAllUsers} = require("../db")
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET
+
+const {getAllUsers, getUserByUsername, createUser} = require("../db")
 
 // userRouter.use((req, res)=>{
 //     console.log("A request made to /users")
@@ -16,6 +21,73 @@ userRouter.get("/", async (req, res)=>{
         console.log(error);
     }
 })
+
+userRouter.post("/login", async (req,res,next)=>{
+    const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      res.send({ message: "you're logged in!" ,
+      token: jwt.sign(user, secret)
+    });
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+userRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, secret, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
+  });
 
 module.exports ={
     userRouter
--- api/index.js
+++ api/index.js
@@ -1,16 +1,69 @@
 const express = require("express");
 const apiRouter = express.Router();
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
+const secret = process.env.JWT_SECRET
+
+const{getUserById} = require("../db")
 
 const {userRouter} = require("./Users");
 const {postRouter} = require("./Posts");
 const {tagRouter} = require("./Tags");
+
+apiRouter.use(async (req, res, next) => {
+    const prefix = 'Bearer ';
+    let auth = "";
+    if(req.header("Authorization")){
+        auth = req.header('Authorization');
+    }
+    if(req.header("authorization")){
+        auth = req.header("authorization")
+    }    
+  
+    if (!auth) { // nothing to see here
+      next();
+    } else if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+  
+      try {
+        const { id } = jwt.verify(token, secret);
+  
+        if (id) {
+          req.user = await getUserById(id);
+          next();
+        }
+      } catch ({ name, message }) {
+        next({ name, message });
+      }
+    } else {
+      next({
+        name: 'AuthorizationHeaderError',
+        message: `Authorization token must start with ${ prefix }`
+      });
+    }
+});
+
+apiRouter.use((req, res, next) => {
+    if (req.user) {
+      console.log("User is set:", req.user);
+    }
+    next();
+});
 
 apiRouter.use('/users', userRouter);
 
 apiRouter.use('/posts', postRouter);
 
 apiRouter.use("/tags", tagRouter);
+
+apiRouter.use((error, req, res, next) => {
+    res.send({
+      name: error.name,
+      message: error.message
+    });
+  });
+  
 
 module.exports = {
     apiRouter
